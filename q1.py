3e9eba68ec943fd47b30c1890cbaa2483a6ba354
jparikh4 <158526490+jparikh4@users.noreply.github.com>
2024-02-14 13:59:15 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,16 @@
-def getPrimeNumbers(n):
+def getprimenumbers(n):
+    "checking if prime number are there or not within given number"""
+    primes = []
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+    return primes
+#Example:
+n = 30
+prime_number= getprimenumbers(n)
+print(f"Prime numbers between 2 and {n}: {prime_number}")
