f81d95c6a1e7364f58083b267b63f3a13c2b759c
jparikh4 <158526490+jparikh4@users.noreply.github.com>
2024-02-14 14:00:07 -0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,19 @@
 def wordCount(t):
+    word_dict = {}
+    with open(t, 'r') as file:
+        for line_number, line in enumerate(file, start=1):
+            words = line.strip().split()
+
+            for word in words:
+                word = word.strip('.,!?').lower()
+
+                if word not in word_dict:
+                    word_dict[word] = []
+                if line_number not in word_dict[word]:
+                    word_dict[word].append(line_number)
+
+    return word_dict
+
+# Example:
+text_file = "word.txt"
+result = wordCount(text_file)
