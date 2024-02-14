b64f3394d66a1f0cb245df4dde1161cd65a9a18b
jparikh4 <158526490+jparikh4@users.noreply.github.com>
2024-02-14 13:59:48 -0500

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,1 +1,26 @@
+import matplotlib.pyplot as plt
+
 def graphSnowfall(t):
+    snowfall_ranges = {'0-10': 0, '11-20': 0, '21-30': 0, '31-40': 0, '41-50': 0}
+
+    with open(t, 'r') as file:
+        for line in file:
+            snowfall = int(line.strip())
+            for key in snowfall_ranges.keys():
+                lower, upper = map(int, key.split('-'))
+                if lower <= snowfall <= upper:
+                    snowfall_ranges[key] += 1
+                    break
+
+    ranges = list(snowfall_ranges.keys())
+    counts = list(snowfall_ranges.values())
+
+    plt.bar(ranges, counts, color='blue')
+    plt.xlabel('Snowfall Range (in cm)')
+    plt.ylabel('Number of Occurrences')
+    plt.title('Snowfall Accumulation Distribution')
+    plt.show()
+
+#Example:
+text_file = 'abc.txt'
+graphSnowfall(text_file)
